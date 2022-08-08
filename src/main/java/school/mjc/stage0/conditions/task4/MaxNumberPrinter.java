c6508bc56c1dd4c3f642aa7cef7a9483dde7622f
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-08 18:36:46 +0600

    first try

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = Math.max(first, second);
+        System.out.println(Math.max(max, third));
     }
 }
